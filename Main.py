76ee7b06e6b342095a334d29eb6d69e1a92e929f
Robert <rpcarff@gmail.com>
2019-09-16 17:59:30 -0400

    adding to main

--- Main.py
+++ Main.py
@@ -11,32 +11,46 @@ degrees = 360 #this is in degrees
 Length = 2000 #this is in milimeters
 driveSpeed = 150 #this is the speed in mm/s
 n = 5 #this is number of sides for the polygon
+sleepTime = 0.0125 #this time is in seconds (12.5 miliseconds)
+####################################################################
+# Button Opcode 165
+# Bit Number:  7	6	5	4	3	2	1	0
+# Bit Number Value: CLOCK SCHEDULE DAY HOUR MINUTE DOCK SPOT CLEAN
+####################################################################
+CLEAN = chr(0)
+SPOT = chr(1)
+DOCK = chr(2)
+MINUTE = chr(3)
+HOUR = chr(4)
+DAY = chr(5)
+SCHEDULE = chr(6)
+CLOCK = chr(7)
 ####################################################################
 # Imports
 import RobotInterface
 ####################################################################
 class main:
 	#drive direct at 150 mm/s speed 
-	def drive():
+	def drive(time):
 		roomba.driveDirect()
-		
+		time.sleep(time)
+
 	#rotating at a specified speed COUNTER CLOCKWISE
 	def rotate():
 		roomba.rotate()
 
 	#calculate the polygon and drive
 	def regularPolygon(n):
-		Angle = degrees/n
+		Angle = degrees - degrees/n
 		sideLength = Length/n
 		driveTime = sideLength/driveSpeed
+		
 		for n
 			#driving for the calculated time
-			drive()
-			time.sleep(driveTime)
-
-
+			drive(driveTime)
 			#rotating specific degree amount
 			rotate()
+			time.sleep(driveTime)
 
 	def main():
 		# (A) initialize a connection
@@ -44,7 +58,7 @@ class main:
 	    # (A) set in passive and safe mode
 	    roomba.setState("SAFE")
 	    roomba.setState("PASSIVE") #I dont think we have passive declared yet
-
-	    # (B) Given an input N move counter clockwise
-	    regularPolygon()
-	    #
+	    if (ReadButton(CLEAN))
+		    # (B) Given an input N move counter clockwise
+		    regularPolygon()
+	    
