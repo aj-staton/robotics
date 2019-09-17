cb942d6ad6ca201dc96607e5aa4c9a5ed57df12d
Robert <rpcarff@gmail.com>
2019-09-17 17:36:27 -0400

    fixing the main and drive method

--- Main.py
+++ Main.py
@@ -9,9 +9,12 @@
 # Magic number Variables
 degrees = 360 #this is in degrees
 Length = 2000 #this is in milimeters
-driveSpeed = 150 #this is the speed in mm/s
 n = 5 #this is number of sides for the polygon
 sleepTime = 0.0125 #this time is in seconds (12.5 miliseconds)
+
+high = 00000000 # calculated for 150 mm/s
+low = 10110110 # calculated for 150 mm/s
+
 ####################################################################
 # Button Opcode 165
 # Bit Number:  7	6	5	4	3	2	1	0
@@ -33,7 +36,7 @@ class main:
 	#drive direct at 150 mm/s speed 
 	def drive(time):
 		#drive direct takes in a high set and low set of bytes
-		roomba.driveDirect()
+		roomba.driveDirect(high,low,high,low)
 		time.sleep(time) #the time is here because the thread
 						 # would run for ever if it was in main
 
@@ -48,7 +51,6 @@ class main:
 		#calculating the lengths and angles
 		Angle = degrees - degrees/n
 		sideLength = Length/n
-
 		#calculating the times for the robot to move
 		driveTime = sideLength/driveSpeed
 		rotateTime = angle/drivespeed
@@ -59,13 +61,15 @@ class main:
 			#rotating specific degree amount
 			rotate(rotateTime)
 
-	def main():
+if __name__ == "__main__":
 		# (A) initialize a connection
 	    roomba = SerialInterface()
 	    # (A) set in passive and safe mode
 	    roomba.setState("SAFE")
 	    roomba.setState("PASSIVE") #I dont think we have passive declared yet
-	    if (ReadButton(CLEAN))
-		    # (B) Given an input N move counter clockwise
-		    regularPolygon()
 	    
+	    '''
+	    if (ReadButton(CLEAN))
+		   # (B) Given an input N move counter clockwise
+		    regularPolygon()
+	    '''
