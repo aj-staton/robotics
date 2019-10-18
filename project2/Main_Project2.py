c12356e1932d1f371adf2ef6afa5294045b4e7a9
Robert <rpcarff@gmail.com>
2019-10-18 10:25:02 -0400

    fixed spaces

--- project2/Main_Project2.py
+++ project2/Main_Project2.py
@@ -50,8 +50,8 @@ lock = Lock() #Initialize lock variable
 # zero turning radius, thus, stopping the robot.
 ###############################################################
 def stopRoomba():
-	roomba.drive(0,0)
-	logging.info("testing")
+    roomba.drive(0,0)
+    logging.info("testing")
 
 ###############################################################
 #  rotate() uses the drive() function, but only rotates
@@ -61,12 +61,12 @@ def stopRoomba():
 #   so turn (135 - 225)
 ###############################################################
 def rotateRandom(direction): #direction is global CW or CCW
-	roomba.drive(_velocity_, direction)
-		# pick a random wait time for 135-225 degrees
-		turnTime = random.randint(_rotateLowTime_,_rotateHighTime_)
-			# turn for that amount of time
-			time.sleep(turnTime)
-			stopRoomba()
+    roomba.drive(_velocity_, direction)
+        # pick a random wait time for 135-225 degrees
+        turnTime = random.randint(_rotateLowTime_,_rotateHighTime_)
+            # turn for that amount of time
+            time.sleep(turnTime)
+            stopRoomba()
 
 ###############################################################
 # mainDrive() continuously checks the boolean values of our
@@ -74,102 +74,102 @@ def rotateRandom(direction): #direction is global CW or CCW
 # taken.
 ###############################################################
 def mainDrive:
-	roomba.setDriving(True) # setting driving to true
-	roomba.drive(_velocity_, _NOROTATE_) # actually driving
+    roomba.setDriving(True) # setting driving to true
+    roomba.drive(_velocity_, _NOROTATE_) # actually driving
 
-	while(True): # while the roomba is driving
+    while(True): # while the roomba is driving
 roomba.sleep(_DELAY_)
 
-	#WE NEED TO ROTATE LEFT OR RIGHT DEPENDING ON BUMPE
-	if(roomba.bumpLeft && roomba.bumpRight):
-		stopRoomba()
-			write(getDistance) # this method write to output
-			rotateRandom(_ROTATECW_)
-			roomba.drive(_velocity_, _NOROTATE_)
+    #WE NEED TO ROTATE LEFT OR RIGHT DEPENDING ON BUMPE
+    if(roomba.bumpLeft && roomba.bumpRight):
+        stopRoomba()
+            write(getDistance) # this method write to output
+            rotateRandom(_ROTATECW_)
+            roomba.drive(_velocity_, _NOROTATE_)
 
 if(roomba.bumpLeft):
-	stopRoomba()
-		write(getDistance) # this method write to output
-		rotateRandom(_ROTATECCW_)
-		roomba.drive(_velocity_, _NOROTATE_)
+    stopRoomba()
+        write(getDistance) # this method write to output
+        rotateRandom(_ROTATECCW_)
+        roomba.drive(_velocity_, _NOROTATE_)
 
-	if(roomba.bumpRight):
-		stopRoomba()
-			write(getDistance) # this method write to output
-			rotateRandom()
-			roomba.drive(_velocity_, _NOROTATE_)
+    if(roomba.bumpRight):
+        stopRoomba()
+            write(getDistance) # this method write to output
+            rotateRandom()
+            roomba.drive(_velocity_, _NOROTATE_)
 
 ###############################################################
 #  readCleanButtonThread() is what checks the iRobot's clean button
 #  during execution. It does this with threading.
 ###############################################################
 '''
-	def readCleanButtonThread():
-	while(True):
-	#global _isDriving_
-	time.sleep(.10)
-	if(roomba.readButton(_CLEAN_, chr(18)) && not(lock.locked())):
-	lock.aquire()
-	#_isDriving_ = not _isDriving_
-	roomba.setDriving()
-	stopRoomba()
-	elif(roomba.readButton(_CLEAN_,chr(18)) && lock.locked()):
-	lock.release()
-	roomba.setDriving()
-	roomba.drive(_velocity_,_NOROTATE_)
-	'''
+    def readCleanButtonThread():
+    while(True):
+    #global _isDriving_
+    time.sleep(.10)
+    if(roomba.readButton(_CLEAN_, chr(18)) && not(lock.locked())):
+    lock.aquire()
+    #_isDriving_ = not _isDriving_
+    roomba.setDriving()
+    stopRoomba()
+    elif(roomba.readButton(_CLEAN_,chr(18)) && lock.locked()):
+    lock.release()
+    roomba.setDriving()
+    roomba.drive(_velocity_,_NOROTATE_)
+    '''
 ###############################################################
 #  readBumperThread() calls our readBumper method in the Interface
 #  the method in the interface already sets out global variables
 ###############################################################
 def readBumperThread():
-	while(True):
-		time.sleep(_DELAY_)
-		# this method sets our 4 global variables
-	roomba.readBumper()
+    while(True):
+        time.sleep(_DELAY_)
+        # this method sets our 4 global variables
+    roomba.readBumper()
 
 ###############################################################
 #  main() controls all actions of execution, including calling
 #  for the drawing of the N-sided polygon for Project 1.
 ###############################################################
 def main():
-	# setting states
-	roomba.setState("SAFE")
-	# declaring threads
-	drive = Thread(target = mainDrive)
-	bump = Thread(target = readBumperThread)
-	# declaring our log file
-	logging.basicConfig(level=logging.DEBUG,filename="output.log",filemode="w")
-	# starting threads
-	drive.start()
-	bump.start()
-	# waiting to start
-	x = True
-	# Listen for the press of the Clean button, which will begin
-	# the drawing of the polygon.
-	while (x):
-	    # ALSO CHECK IF THE WHEEL DROPS AND CLIFFS ARE ACTIVATED
-	    if(roomba.readButton(_CLEAN_)):
-	        x = False
-	while(True):
-	    time.sleep(_DELAY_)
-	    if(roomba.readButton(_CLEAN_) && not(lock.locked())):
-		lock.aquire()
-		roomba.setDriving()
-		stopRoomba()
-	    elif(roomba.readButton(_CLEAN_) && lock.locked()):
-		lock.release()
-		roomba.setDriving()
-		roomba.drive(_velocity_,_NOROTATE_)				
-		stopRoomba()
-	        #mainDrive() #drive and turn a bunch
-				    
-		# end our threads and stop the roomba
-		drive.join()
-		bump.join()
-		stopRoomba()
-		sys.exit()
-
+    # setting states
+    roomba.setState("SAFE")
+    # declaring threads
+    drive = Thread(target = mainDrive)
+    bump = Thread(target = readBumperThread)
+    # declaring our log file
+    logging.basicConfig(level=logging.DEBUG,filename="output.log",filemode="w")
+    # starting threads
+    drive.start()
+    bump.start()
+    # waiting to start
+    x = True
+    # Listen for the press of the Clean button, which will begin
+    # the drawing of the polygon.
+    while (x):
+        # ALSO CHECK IF THE WHEEL DROPS AND CLIFFS ARE ACTIVATED
+        if(roomba.readButton(_CLEAN_)):
+            x = False
+    while(True):
+        time.sleep(_DELAY_)
+        if(roomba.readButton(_CLEAN_) && not(lock.locked())):
+        lock.aquire()
+        roomba.setDriving()
+        stopRoomba()
+        elif(roomba.readButton(_CLEAN_) && lock.locked()):
+        lock.release()
+        roomba.setDriving()
+        roomba.drive(_velocity_,_NOROTATE_)             
+        stopRoomba()
+            #mainDrive() #drive and turn a bunch
+                    
+        # end our threads and stop the roomba
+        drive.join()
+        bump.join()
+        stopRoomba()
+        sys.exit()
+#fixed spaces
 
 if __name__ == __main__:
     main()
