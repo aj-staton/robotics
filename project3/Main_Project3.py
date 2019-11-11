996033993b8c3dc2bda16c534810b795b0afa2b3
Robert <rpcarff@gmail.com>
2019-11-11 18:03:51 -0500

    really bad

--- project3/Main_Project3.py
+++ project3/Main_Project3.py
@@ -127,9 +127,9 @@ def readSensors():
         roomba.readSensors()
         driveLogic()
         right = roomba.readInfraredRight()
-        print("RIGHT: " + string(roomba.rightIRSensor))
+        print("RIGHT: " + str(roomba.rightIRSensor))
         left = roomba.readInfraredLeft()
-        print("LEFT: " + string(roomba.leftIRSensor))
+        print("LEFT: " + str(roomba.leftIRSensor))
 
 ###############################################################
 #  main() controls all actions of execution, including calling
